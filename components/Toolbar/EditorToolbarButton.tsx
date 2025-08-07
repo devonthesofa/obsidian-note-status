16299447381edfd8a1828cf6a78d64dc9840c70d
Aleix Soler <aleixsoler64@gmail.com>
2025-08-07 20:02:23 +0200

    chore: improve UI

--- components/Toolbar/EditorToolbarButton.tsx
+++ components/Toolbar/EditorToolbarButton.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo, useEffect, useState } from "react";
+import React, { FC, memo } from "react";
 import { GroupedStatuses } from "@/types/noteStatus";
 import { StatusesInfoPopup } from "@/integrations/popups/statusesInfoPopupIntegration";
 
@@ -13,19 +13,11 @@ interface EditorToolbarButtonProps {
 
 export const EditorToolbarButton: FC<EditorToolbarButtonProps> = memo(
 	({ statuses, onClick, unknownStatusConfig }) => {
-		const [statusChanged, setStatusChanged] = useState(false);
 		const statusEntries = Object.entries(statuses);
 		const allStatuses = statusEntries.flatMap(
 			([_, statusList]) => statusList,
 		);
 		const totalStatuses = allStatuses.length;
-
-		// Animate when status changes
-		useEffect(() => {
-			setStatusChanged(true);
-			const timer = setTimeout(() => setStatusChanged(false), 600);
-			return () => clearTimeout(timer);
-		}, [statusEntries.length, allStatuses.map((s) => s.name).join(",")]);
 
 		const handleMouseEnter = () => {
 			StatusesInfoPopup.open(statuses);
@@ -37,27 +29,15 @@ export const EditorToolbarButton: FC<EditorToolbarButtonProps> = memo(
 
 		const getPrimaryStatus = () => allStatuses[0];
 
-		const containerClasses = [
-			"editor-toolbar-button",
-			totalStatuses === 0
-				? "editor-toolbar-button--no-status"
-				: "editor-toolbar-button--has-status",
-			totalStatuses > 1 ? "editor-toolbar-button--multiple" : "",
-			statusChanged ? "editor-toolbar-button--status-changed" : "",
-		]
-			.filter(Boolean)
-			.join(" ");
-
 		// No status state
 		if (totalStatuses === 0) {
 			return (
 				<button
 					type="button"
-					className={containerClasses}
+					className="clickable-icon"
 					onClick={onClick}
 					onMouseEnter={handleMouseEnter}
 					onMouseLeave={handleMouseLeave}
-					title="No status assigned - click to add"
 					aria-label="Add status to note"
 				>
 					<span
@@ -65,9 +45,6 @@ export const EditorToolbarButton: FC<EditorToolbarButtonProps> = memo(
 						style={{ color: unknownStatusConfig.color }}
 					>
 						{unknownStatusConfig.icon}
-					</span>
-					<span className="editor-toolbar-button__text">
-						Add Status
 					</span>
 				</button>
 			);
@@ -80,22 +57,14 @@ export const EditorToolbarButton: FC<EditorToolbarButtonProps> = memo(
 			return (
 				<button
 					type="button"
-					className={containerClasses}
+					className="clickable-icon"
 					onClick={onClick}
 					onMouseEnter={handleMouseEnter}
 					onMouseLeave={handleMouseLeave}
-					title={`Status: ${primaryStatus.name} - click to change`}
 					aria-label={`Current status: ${primaryStatus.name}. Click to change.`}
-					style={
-						{
-							"--status-color":
-								primaryStatus.color ||
-								"var(--interactive-accent)",
-						} as React.CSSProperties
-					}
 				>
 					<span
-						className="editor-toolbar-button__icon"
+						className="editor-toolbar-button__icon editor-toolbar-button__icon--has-status"
 						style={{
 							color:
 								primaryStatus.color ||
@@ -103,9 +72,6 @@ export const EditorToolbarButton: FC<EditorToolbarButtonProps> = memo(
 						}}
 					>
 						{primaryStatus.icon || "📝"}
-					</span>
-					<span className="editor-toolbar-button__text">
-						{primaryStatus.name}
 					</span>
 				</button>
 			);
@@ -115,22 +81,15 @@ export const EditorToolbarButton: FC<EditorToolbarButtonProps> = memo(
 		return (
 			<button
 				type="button"
-				className={containerClasses}
+				className="clickable-icon"
 				onClick={onClick}
 				onMouseEnter={handleMouseEnter}
 				onMouseLeave={handleMouseLeave}
-				title={`${totalStatuses} statuses: ${allStatuses.map((s) => s.name).join(", ")} - click to change`}
 				aria-label={`${totalStatuses} statuses assigned. Click to change.`}
-				style={
-					{
-						"--status-color":
-							primaryStatus.color || "var(--interactive-accent)",
-					} as React.CSSProperties
-				}
 			>
-				<span className="editor-toolbar-button__icon-stack">
+				<div className="editor-toolbar-button__icon-container">
 					<span
-						className="editor-toolbar-button__icon"
+						className="editor-toolbar-button__icon editor-toolbar-button__icon--has-status"
 						style={{
 							color:
 								primaryStatus.color ||
@@ -142,10 +101,7 @@ export const EditorToolbarButton: FC<EditorToolbarButtonProps> = memo(
 					<span className="editor-toolbar-button__counter">
 						{totalStatuses}
 					</span>
-				</span>
-				<span className="editor-toolbar-button__text">
-					{totalStatuses} Status{totalStatuses === 1 ? "" : "es"}
-				</span>
+				</div>
 			</button>
 		);
 	},
